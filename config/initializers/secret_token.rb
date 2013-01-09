1bba5a8796b72ff33a753cb39324d420d8ef61fa
Kiran Soumya <kiran.soumya@gmail.com>
2013-01-09 12:28:21 +0530

    first commit with devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PocketEnterprise::Application.config.secret_token = '9fb93a6195c5da26d6b5e92567db311dc0cdb0dc05ec1f0ba0dec3904cf9b21ab9de18d6d295fce905d1dc3d1d39326440c81b1a036b55261f11941f86a9084d'
