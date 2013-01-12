f97595c1b960541da81b784dcc183733694186ee
Kiran Soumya <kiran.soumya@gmail.com>
2013-01-12 19:30:50 +0530

    initial login done

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,12 @@
 class WelcomeController < ApplicationController
+	before_filter :authenticate_user!
 	def index
-		render :text => "hello world"
+	  @greetings = Hash.new
+	  @greetings[:hello] = "world"
+      respond_to do |format|
+        format.html
+        format.xml { render :xml => @greetings }
+        format.json { render :json => @greetings.to_json }
+      end
 	end
 end
